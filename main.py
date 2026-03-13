e30553f2dcb00306c30424d303ad9dfbd2e711ca
Daniele Aliani <daniele.aliani@gmail.com>
2026-03-13 17:52:49 +0100

    refactor: modularize trivia game logic into functions

--- main.py
+++ main.py
@@ -19,34 +19,66 @@ questions = {
 }
 
 
-def main():
-    points_counter = 0
+def print_header():
     print("\n=== TRIVIA GAME 1.0 ===")
     print("press q to exit\n")
 
-    q_selected = random.sample(list(questions.keys()), N_QUESTION)
-    for i, question in enumerate(q_selected):
-        answer = input(f"Question {i + 1}: {question} ").lower().strip()
 
-        if answer == "q":
-            print("=== CLOSING GAME ===")
-            return
+def get_answer(i, question):
+    return input(f"Question {i + 1}: {question} ").lower().strip()
 
-        if answer == questions[question].lower():
-            print(f"correct! +{POINT_STEPS}")
-            points_counter += POINT_STEPS
-            print(f"you now have {points_counter}/{MAX_POINTS} points\n")
 
-            if points_counter == MAX_POINTS:
-                print("YOU WIN!")
-                print("=== CLOSING GAME ===")
-                return
-        else:
-            print(f"Wrong! the correct anwer is: {questions[question]}")
-            print(f"you now have {points_counter}/{MAX_POINTS} points\n")
+def check_answer(user_answer, question, points_counter):
+    if user_answer == "q":
+        print("=== CLOSING GAME ===")
+        return None
 
+    if user_answer == questions[question].lower():
+        print(f"correct! +{POINT_STEPS}")
+        print(f"you now have {points_counter + POINT_STEPS}/{MAX_POINTS} points\n")
+        return POINT_STEPS
+
+    else:
+        print(f"Wrong! the correct anwer is: {questions[question]}")
+        print(f"you still have {points_counter}/{MAX_POINTS} points\n")
+        return 0
+
+
+def check_vicotry(points_counter):
+    if points_counter == MAX_POINTS:
+        return 1
+
+
+def print_win():
+    print("YOU WIN!")
+    print("=== CLOSING GAME ===")
+
+
+def print_lose(points_counter):
     print(f"You lose!, yuor score is {points_counter}/{MAX_POINTS}")
     print("=== CLOSING GAME ===")
+
+
+def main():
+
+    print_header()
+    points_counter = 0
+    qs_selected = random.sample(list(questions.keys()), N_QUESTION)
+
+    for i, question in enumerate(qs_selected):
+        user_answer = get_answer(i, question)
+        result = check_answer(user_answer, question, points_counter)
+
+        if result is None:
+            return
+
+        points_counter += result
+
+        if check_vicotry(points_counter):
+            print_win()
+            return
+
+    print_lose(points_counter)
 
 
 if __name__ == "__main__":
