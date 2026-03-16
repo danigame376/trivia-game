cb8ea02226029ee96b6e7d415ebe4a4950d7b7ff
Daniele Aliani <daniele.aliani@gmail.com>
2026-03-17 00:15:00 +0100

    refactor: rewrite of code using OOP

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
 import random
+import sys
 import json
+from pathlib import Path
 
-N_QUESTION = 5
+N_QUESTIONS = 5
 MAX_POINTS = 3
 POINT_STEPS = 1
 FILENAME = "questions.json"
 
 
-with open(FILENAME, "r", encoding="utf-8") as file:
-    questions = json.load(file)
+class TriviaGame:
+    def __init__(
+        self, filename: str, point_steps: int, max_points: int, n_questions: int
+    ) -> None:
+        self.filepath = Path(filename)
+        self.questions = self._load_questions()
+        self.points = 0
+        self.point_steps = point_steps
+        self.max_points = max_points
+        self.n_questions = n_questions
+
+    def _load_questions(self) -> dict[str, str]:
+        if not self.filepath.exists():
+            print(f"Non trovo il file {self.filepath}")
+            print("=== CLOSING GAME... ===")
+            sys.exit(1)
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            print(f"il file {self.filepath} non è un jason valido")
+            print("=== CLOSING GAME... ===")
+            sys.exit(1)
+
+    def _get_answer(self, i: int, question: str) -> str:
+        return input(f"Question {i}: {question} ").lower().strip()
+
+    def _check_user_answer(self, answer: str, question: str) -> bool:
+        if answer == "q":
+            return True
+
+        if answer == self.questions[question].lower():
+            self.points += self.point_steps
+            print(f"Correct! you have {self.points}/{self.max_points} points")
+        else:
+            print(f"Wrong, you still have {self.points}/{self.max_points} points")
+        return False
+
+    def run(self) -> None:
+        print("=== TRIVIA GAME 1.0 ===")
+        print("Press q to exit")
+
+        n_questions = min(self.n_questions, len(self.questions))
+        qs_selected = random.sample(list(self.questions.keys()), n_questions)
+
+        for i, question in enumerate(qs_selected, start=1):
+            user_answer = self._get_answer(i, question)
+            user_wants_to_quit = self._check_user_answer(user_answer, question)
+            if user_wants_to_quit:
+                print("=== CLOSING GAME... ===")
+                return
+            if self.points == self.max_points:
+                print("YOU WIN!")
+                print("=== CLOSING GAME... ===")
+                return
+        print("You lose!")
+        print("=== CLOSING GAME... ===")
+        return
 
 
-def print_header():
-    print("\n=== TRIVIA GAME 1.0 ===")
-    print("press q to exit\n")
-
-
-def get_answer(i, question):
-    return input(f"Question {i + 1}: {question} ").lower().strip()
-
-
-def check_answer(user_answer, question, points_counter):
-    if user_answer == "q":
-        print("=== CLOSING GAME ===")
-        return None
-
-    if user_answer == questions[question].lower():
-        print(f"correct! +{POINT_STEPS}")
-        print(f"you now have {points_counter + POINT_STEPS}/{MAX_POINTS} points\n")
-        return POINT_STEPS
-
-    else:
-        print(f"Wrong! the correct answer is: {questions[question]}")
-        print(f"you still have {points_counter}/{MAX_POINTS} points\n")
-        return 0
-
-
-def check_vicotry(points_counter):
-    if points_counter == MAX_POINTS:
-        return 1
-
-
-def print_win():
-    print("YOU WIN!")
-    print("=== CLOSING GAME ===")
-
-
-def print_lose(points_counter):
-    print(f"You lose!, your score is {points_counter}/{MAX_POINTS}")
-    print("=== CLOSING GAME ===")
-
-
-def main():
-
-    print_header()
-    points_counter = 0
-    qs_selected = random.sample(list(questions.keys()), N_QUESTION)
-
-    for i, question in enumerate(qs_selected):
-        user_answer = get_answer(i, question)
-        result = check_answer(user_answer, question, points_counter)
-
-        if result is None:
-            return
-
-        points_counter += result
-
-        if check_vicotry(points_counter):
-            print_win()
-            return
-
-    print_lose(points_counter)
+def main() -> None:
+    game = TriviaGame(FILENAME, POINT_STEPS, MAX_POINTS, N_QUESTIONS)
+    game.run()
 
 
 if __name__ == "__main__":
